49719ef79b1c85db9e5b5a7e9cbe79378bae302b
30025278 <951425852hl@gmail.com>
2025-11-22 20:21:51 +0800

    zuoye6

---
+++ homework/go1/removeDuplicates6.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	k := removeDuplicates(nums)
+	fmt.Println(k)
+}
